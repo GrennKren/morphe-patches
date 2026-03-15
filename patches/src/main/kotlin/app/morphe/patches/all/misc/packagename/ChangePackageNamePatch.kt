e15d4cd8f96857a477a98edffee7821fe3c0c54b
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-03-15 08:30:49 +0100

    chore: Update outdated Manager check

--- patches/src/main/kotlin/app/morphe/patches/all/misc/packagename/ChangePackageNamePatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/all/misc/packagename/ChangePackageNamePatch.kt
@@ -171,25 +171,22 @@ val changePackageNamePatch = resourcePatch(
 
     dependsOn(bytecodePatch {
         execute {
-            // TODO: Eventually remove this check. Early versions of Morphe Manager
-            //       may not auto update if GitHub non auth API blocks the user ip.
             try {
-                //noinspection CheckResult
-                PackageMetadata::class.java.getDeclaredField("packageName")
-            } catch (e: Exception) {
+                when (val originalPackageName = packageMetadata.packageName) {
+                    PACKAGE_NAME_REDDIT -> {
+                        applyGetPackageName(
+                            originalPackageName,
+                            "Lcom/google/android/recaptcha/internal"
+                        )
+                    }
+                }
+            } catch (e: Throwable) {
+                // TODO: Eventually remove this check. Early versions of Morphe Manager
+                //       may not auto update if GitHub non auth API blocks the user ip.
                 throw RuntimeException(
                     "Your Morphe app is outdated. Please manually update Morphe " +
                             "by downloading from https://morphe.software\n\n"
                 )
-            }
-
-            when (val originalPackageName = packageMetadata.packageName) {
-                PACKAGE_NAME_REDDIT -> {
-                    applyGetPackageName(
-                        originalPackageName,
-                        "Lcom/google/android/recaptcha/internal"
-                    )
-                }
             }
         }
     })
