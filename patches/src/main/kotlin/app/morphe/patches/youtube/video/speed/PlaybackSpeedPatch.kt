7e7e08dbfff859890b3bce26fcc882cff1074f4a
MarcaDian <152095496+MarcaDian@users.noreply.github.com>
2026-03-17 18:40:12 +0200

    feat(YouTube Music): Add `Miniplayer previous and next buttons` patch (#818)

--- patches/src/main/kotlin/app/morphe/patches/youtube/video/speed/PlaybackSpeedPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/youtube/video/speed/PlaybackSpeedPatch.kt
@@ -14,14 +14,11 @@ import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.patches.shared.misc.settings.preference.BasePreference
 import app.morphe.patches.shared.misc.settings.preference.PreferenceCategory
 import app.morphe.patches.shared.misc.settings.preference.PreferenceScreenPreference.Sorting
-import app.morphe.patches.youtube.misc.playservice.is_20_40_or_greater
-import app.morphe.patches.youtube.misc.playservice.versionCheckPatch
 import app.morphe.patches.youtube.misc.settings.PreferenceScreen
 import app.morphe.patches.youtube.shared.Constants.COMPATIBILITY_YOUTUBE
 import app.morphe.patches.youtube.video.speed.button.playbackSpeedButtonPatch
 import app.morphe.patches.youtube.video.speed.custom.customPlaybackSpeedPatch
 import app.morphe.patches.youtube.video.speed.remember.rememberPlaybackSpeedPatch
-import app.morphe.util.insertLiteralOverride
 
 /**
  * Speed menu settings. Used to organize all speed related settings together.
