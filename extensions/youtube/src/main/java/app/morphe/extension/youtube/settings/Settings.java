ca21b8b90e59b1b8e0f603127ac1a47375e0145c
naufal <miosan389@gmail.com>
2026-03-18 12:04:04 +0800

    fix(settings): change default playlist autonext blocking to disabled
    The BLOCK_PLAYLIST_AUTONEXT setting default value was changed from TRUE to FALSE, making playlist autonext blocking disabled by default while keeping the feature available for users to enable manually.

--- extensions/youtube/src/main/java/app/morphe/extension/youtube/settings/Settings.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/settings/Settings.java
@@ -525,7 +525,7 @@ public class Settings extends SharedYouTubeSettings {
     private static final BooleanSetting DEPRECATED_SEEKBAR_TAPPING = new BooleanSetting("morphe_seekbar_tapping", FALSE);
     private static final BooleanSetting DEPRECATED_DISABLE_RESUMING_SHORTS_PLAYER = new BooleanSetting("morphe_disable_resuming_shorts_player", FALSE);
 
-    public static final BooleanSetting BLOCK_PLAYLIST_AUTONEXT = new BooleanSetting("morphe_block_playlist_autonext", TRUE, false);
+    public static final BooleanSetting BLOCK_PLAYLIST_AUTONEXT = new BooleanSetting("morphe_block_playlist_autonext", FALSE, false);
     public static final BooleanSetting BLOCK_PLAYLIST_AUTONEXT_BUTTON = new BooleanSetting("morphe_block_playlist_autonext_button", TRUE);
 
     static {
